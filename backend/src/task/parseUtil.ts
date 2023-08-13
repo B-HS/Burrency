4eb28e0b02d1f8a5e031bdaf1b7cdf297e59d416
Hyunseok Byun <gumyoincirno@gmail.com>
2023-08-13 18:53:05 +0900

    [BACK/etc] Fix .gitignore, and etc.

--- backend/src/task/parseUtil.ts
+++ backend/src/task/parseUtil.ts
@@ -28,8 +28,28 @@ export const parseHTML = (html: string) => {
         }
     });
 
-    let USD = exchangeData.find(val=>val.country ==="USD").AVG
-    let JPY = exchangeData.find(val=>val.country ==="JPY").AVG
+    /**
+     * 
+     * exchangeData.country = [
+  'USD', 'EUR', 'JPY', 'CNY', 'HKD',   'TWD',
+  'GBP', 'OMR', 'CAD', 'CHF', 'SEK',   'AUD',
+  'NZD', 'CZK', 'CLP', 'TRY', 'MNT',   'ILS',
+  'DKK', 'NOK', 'SAR', 'KWD', 'BHD',   'AED',
+  'JOD', 'EGP', 'THB', 'SGD', 'MYR',   'IDR',
+  'QAR', 'KZT', 'BND', 'INR', 'PKR',   'BDT',
+  'PHP', 'MXN', 'BRL', 'VND', '��ȭ��', 'RUB',
+  'HUF', 'PLN', 'LKR', 'DZD', 'KES',   'COP',
+  'TZS', 'NPR', 'RON', 'LYD', 'MOP',   'MMK',
+  'ETB', 'UZS', 'KHR', 'FJD'
+]
+     * 
+     */
+
+    let USD = exchangeData.find(val => val.country === "USD").AVG
+    let JPY = exchangeData.find(val => val.country === "JPY").AVG
+
+    console.log(exchangeData.map(val => val.country));
+
 
     return { USD, JPY };
 }
