869d9b87b8490654a5d34f4cbab9aa8a72d96e61
Hyunseok Byun <gumyoincirno@gmail.com>
2023-08-28 01:08:42 +0900

    [ETC/PUBLISH] Set publishers line

--- frontend/forge.config.ts
+++ frontend/forge.config.ts
@@ -11,9 +11,22 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
+        icon: '/src/assets/IconTemplate.png'
     },
     rebuildConfig: {},
     makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'B-HS',
+                    name: 'Burrency'
+                },
+                prerelease: true
+            }
+        }
+    ],
     plugins: [
         new AutoUnpackNativesPlugin({}),
         new WebpackPlugin({
